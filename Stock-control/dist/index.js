df0854f00bf7ac25cb63eaa9413e7398f53fd6f2
pablo0261 <pablobesler@gmail.com>
2024-03-08 23:19:33 -0300

    configurando typescript

---
+++ Stock-control/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const numbers = 42;
+const arraynumber = [1, 2, 3, 4, 5];
+const palabra = "pablo";
+arraynumber.push(2, 3, 4);
+console.log(`Numeros en el arreglo ${numbers}`);
---
+++ Stock-control/index.ts
@@ -0,0 +1,9 @@
+const numbers: number =  42;
+
+const arraynumber: number[] = [1,2,3,4,5]
+
+const palabra: string = "pablo"
+
+arraynumber.push(2,3,4)
+
+console.log(`Numeros en el arreglo ${numbers}`)
