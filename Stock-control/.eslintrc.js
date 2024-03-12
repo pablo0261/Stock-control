f7abc374fcbe23a9c6d70fa3e9727a77a9100cde
pablo0261 <pablobesler@gmail.com>
2024-03-12 01:21:54 -0300

    nueva estructura con vite

--- Stock-control/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-const eslintConfig = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  rules: {}
-};
-
-export default eslintConfig;
