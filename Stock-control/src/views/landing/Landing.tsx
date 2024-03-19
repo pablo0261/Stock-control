599dd2e0fff36a0c66af30a4d9215317bae44fad
pablo0261 <pablobesler@gmail.com>
2024-03-19 01:30:26 -0300

    intento de aplicar transition desde landing hasta login pero aun no funciona

--- Stock-control/src/views/landing/Landing.tsx
+++ Stock-control/src/views/landing/Landing.tsx
@@ -1,29 +1,59 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { Transition } from "react-transition-group";
+import animationStock from "../../../utils/AnimationStock.gif";
+import style from "./Landing.module.sass";
+
+const transitionDuration: number = 900;
+
+const transitionStyles: { [key: string]: React.CSSProperties } = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1, transition: `opacity ${transitionDuration}ms ease-in-out` },
+};
 
 const LandingView: React.FC = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true); 
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsLoading(false); 
-      navigate('/LogIn');
-    }, 4000);
+      setIsLoading(false);
+    }, 3000);
 
-    return () => clearTimeout(timer); 
-  }, [navigate]);
+    return () => clearTimeout(timer);
+  }, []);
 
-  
+  useEffect(() => {
+    if (!isLoading) {
+      navigate("/LogIn");
+    }
+  }, [isLoading, navigate]);
+
   return (
-    <>
-      {isLoading ? (
-        <div>Cargando...</div>
-      ) : (
-        <div> landingView</div>
-      )}
-    </>
+    <div className={style.background}>
+      <Transition in={!isLoading} timeout={transitionDuration}>
+        {(state: string) => (
+          <div
+            style={{
+              ...transitionStyles[state],
+            }}
+          >
+            <div className={style.marginContainer}>
+              {isLoading ? (
+                <img
+                  className={style.loading}
+                  src={animationStock}
+                  alt="Loading..."
+                />
+              ) : (
+                <div>landingView</div>
+              )}
+            </div>
+          </div>
+        )}
+      </Transition>
+    </div>
   );
-}
+};
 
 export default LandingView;
